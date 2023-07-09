baa5ad5efad0f68ab53d5c2379bb868dfb73fa6b
ShathisKannan19 <110104103+ShathisKannan19@users.noreply.github.com>
2023-07-09 18:19:23 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {WeatherApp} from './Components/WeatherApp'
+
+export const App = ()=> {
+
+  return (
+    <>
+      <WeatherApp/>
+    </>
+  )
+}
---
+++ src/Components/WeatherApp.jsx
@@ -0,0 +1,234 @@
+import one from './p13.jpeg';
+import './iit.css'
+import two from './My project.png';
+
+export const WeatherApp = () => {
+	return(
+		<>
+		<div className="frame-1">
+  <div className="rectangle-3"></div>
+
+  <img className="rectangle-1" src={two} />
+
+  <div className="new-york-usa">New York, USA</div>
+
+  <div className="today-08-july">Today 08,July</div>
+
+  <div className="_07-28-pm">07:28 PM</div>
+
+  <div className="f">°F</div>
+
+  <div className="_19-28-pm">19:28 PM</div>
+
+  <div className="rectangle-2"></div>
+
+  <div className="welcome-back-ram">Welcome back Ram!</div>
+
+  <div className="more-details-of-today-s-weather">
+    More details of today’s weather
+  </div>
+
+  <div className="check-out-today-s-weather-information">
+    Check out today’s weather information
+  </div>
+
+  <div className="rectangle-5"></div>
+
+  <div className="rectangle-6"></div>
+
+  <div className="rectangle-8"></div>
+
+  <div className="rectangle-10"></div>
+
+  <div className="rectangle-7"></div>
+
+  <div className="rectangle-9"></div>
+
+  <div className="rectangle-11"></div>
+
+  <div className="rectangle-13"></div>
+
+  <div className="rectangle-14"></div>
+
+  <div className="bad">Bad</div>
+
+  <div className="upcoming-hours">Upcoming hours</div>
+
+  <div className="rain-precipitation">Rain Precipitation</div>
+
+  <div className="next-day">Next Day &gt;</div>
+
+  <div className="now">Now</div>
+
+  <div className="_27">27°</div>
+
+  <div className="_23">23%</div>
+
+  <div className="_29">29%</div>
+
+  <div className="_82">82%</div>
+
+  <div className="medium">medium</div>
+
+  <div className="_4">4</div>
+
+  <div className="_42">42%</div>
+
+  <div className="_8-km-h">8 km/h</div>
+
+  <div className="_1-4-cm">1.4 cm</div>
+
+  <div className="_58">58%</div>
+
+  <div className="_75">75%</div>
+
+  <div className="_33">33%</div>
+
+  <div className="_20">20%</div>
+
+  <div className="_73">73%</div>
+
+  <div className="_49">49%</div>
+
+  <div className="_28">28°</div>
+
+  <div className="_282">28°</div>
+
+  <div className="_292">29°</div>
+
+  <div className="_30">30°</div>
+
+  <div className="_293">29°</div>
+
+  <div className="_294">29°</div>
+
+  <div className="_283">28°</div>
+
+  <div className="wb-sunny">wb_sunny</div>
+
+  <div className="wb-sunny2"><i className="material-icons icon">water_drop</i></div>
+
+  <div className="wb-sunny3">wb_sunny</div>
+
+  <div className="cloud">cloud</div>
+
+  <div className="cloud2">cloud</div>
+
+  <div className="wb-sunny4">wb_sunny</div>
+
+  <div className="cloud3">cloud</div>
+
+  <div className="cloud4">cloud</div>
+
+  <div className="wb-sunny5">wb_sunny</div>
+
+  <div className="now2">12:00</div>
+
+  <div className="now3">13:00</div>
+
+  <div className="now4">14:00</div>
+
+  <div className="now5">15:00</div>
+
+  <div className="now6">16:00</div>
+
+  <div className="now7">11:00</div>
+
+  <div className="now8">17:00</div>
+
+  <div className="_272">27°</div>
+
+  <svg
+    className="location-on"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_2_41)">
+      <path
+        d="M7.99998 1.33334C5.41998 1.33334 3.33331 3.42001 3.33331 6.00001C3.33331 9.50001 7.99998 14.6667 7.99998 14.6667C7.99998 14.6667 12.6666 9.50001 12.6666 6.00001C12.6666 3.42001 10.58 1.33334 7.99998 1.33334ZM7.99998 7.66668C7.07998 7.66668 6.33331 6.92001 6.33331 6.00001C6.33331 5.08001 7.07998 4.33334 7.99998 4.33334C8.91998 4.33334 9.66665 5.08001 9.66665 6.00001C9.66665 6.92001 8.91998 7.66668 7.99998 7.66668Z"
+        fill="white"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_2_41">
+        <rect width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+
+  <div className="add-box">add_box</div>
+
+  <div className="rectangle-12"></div>
+
+  <div className="ellipse-1"></div>
+
+  <div className="humidity">Humidity</div>
+
+  <div className="wb-sunny6"><i className="material-icons icon">air</i></div>
+
+  <div className="wind">Wind</div>
+
+  <div className="wb-sunny7"><i className="material-icons icon">cloud_queue</i></div>
+
+  <div className="preception">Preception</div>
+
+  <div className="wb-sunny8"><i className="material-icons icon">beach_access</i></div>
+
+  <div className="change-of-rain">Change of rain</div>
+
+  <div className="wb-sunny9"><i className="material-icons icon">thermostat</i></div>
+
+  <div className="feels-like">Feels Like</div>
+
+  <div className="wb-sunny10"><i className="material-icons icon">wb_sunny</i></div>
+
+  <div className="uv-index">UV Index</div>
+
+  <div className="rectangle-15"></div>
+
+  <div className="component-1">
+    <div className="rectangle-24"></div>
+
+    <div className="rectangle-25"></div>
+  </div>
+
+  <div className="component-4">
+    <div className="rectangle-24"></div>
+
+    <div className="rectangle-25"></div>
+  </div>
+
+  <div className="component-2">
+    <div className="rectangle-24"></div>
+
+    <div className="rectangle-25"></div>
+  </div>
+
+  <div className="component-3">
+    <div className="rectangle-24"></div>
+
+    <div className="rectangle-25"></div>
+  </div>
+
+  <div className="rectangle-16"></div>
+
+  <div className="rectangle-17"></div>
+
+  <div className="rectangle-18"></div>
+
+  <div className="rectangle-19"></div>
+
+  <div className="rectangle-20"></div>
+
+  <div className="rectangle-21"></div>
+
+  <div className="rectangle-22"></div>
+
+  <img className="rectangle-23" src={one} />
+</div>
+		</>
+		)
+}
